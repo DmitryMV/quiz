6a1818e6d7dab17354fe51ec13b34a62eb5bf7f5
Dmitry Mitrofanov <dmitrymv@gmail.com>
2020-05-27 23:00:05 +0300

    Quiz implementation

---
+++ src/main/java/com/luxoft/leadership/Question.java
@@ -0,0 +1,24 @@
+package com.luxoft.leadership;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Question {
+    public String text;
+    public Set<String> options=new HashSet<>();
+    public Set<String> answers=new HashSet<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Question question1 = (Question) o;
+        return Objects.equals(text, question1.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(text);
+    }
+}
---
+++ src/main/java/com/luxoft/leadership/Quiz.java
@@ -0,0 +1,143 @@
+package com.luxoft.leadership;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static java.lang.String.format;
+
+public class Quiz {
+
+
+    public static final Path QUESTIONS_POOL_FILE = Paths.get("questionsPool.json");
+
+    public static void main(String[] args) throws IOException {
+        Quiz quiz = new Quiz();
+        quiz.start();
+    }
+
+    private final Map<String, Question> questionsPool = new HashMap<>();
+
+    public void start() throws IOException {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
+            loadKnowledgeBase();
+            while (true) {
+                System.out.println("New Quiz");
+                startQuiz(in);
+            }
+        }
+    }
+
+    public void startQuiz(BufferedReader in) throws IOException {
+        String line;
+        System.out.println("Enter question followed by empty line or 'end quiz' to review quiz:");
+        while ((line = in.readLine()) != null) {
+            if ("end quiz".equalsIgnoreCase(line)) {
+                review(in);
+                saveKnowledgeBase();
+                return;
+            } else {
+                readQuestion(line, in);
+            }
+            System.out.println("Enter question followed by empty line or 'end quiz' to review quiz:");
+        }
+    }
+
+    private void readQuestion(String questionText, BufferedReader in) throws IOException {
+        Set<String> options = new LinkedHashSet<>();
+        String line = null;
+        while ((line = in.readLine()) != null && "".equals(line.trim())) {
+            // skip empty lines
+        }
+
+        while ((line = in.readLine()) != null) {
+            line = line.trim();
+            if ("".equalsIgnoreCase(line)) {
+                break;
+            } else {
+                options.add(line);
+            }
+        }
+        Question question = questionsPool.computeIfAbsent(questionText, (q) -> new Question());
+        question.text = questionText;
+        question.options.addAll(options);
+        if (!question.answers.isEmpty()) {
+            System.out.println("Correct answers:");
+            for (String answer : question.answers) {
+                System.out.println(format("  %s", answer));
+            }
+            System.out.println("hit ENTER to proceed or 'change' to change answer");
+            line = in.readLine();
+            if ("".equalsIgnoreCase(line)) {
+                System.out.println();
+                return;
+            }
+        }
+        System.out.println("Pick answer:");
+        String[] optionsArr = options.toArray(new String[0]);
+        int i = 1;
+        for (String option : optionsArr) {
+            System.out.println(format("  %d: %s", i++, option));
+        }
+        System.out.print("Answer # (comma separated): ");
+        String[] indxes = in.readLine().split(",");
+        System.out.println("You have selected options:");
+        Set<String> selectedAnswers = new LinkedHashSet<>();
+        for (String indx : indxes) {
+            int idx = Integer.parseInt(indx.trim());
+            String selected = optionsArr[idx - 1];
+            System.out.println(format("  %s: %s", indx, selected));
+            selectedAnswers.add(selected);
+        }
+        question.answers = selectedAnswers;
+        questionsPool.put(questionText, question);
+        System.out.println();
+    }
+
+    private void review(BufferedReader in) throws IOException {
+        System.out.println("List incorrect questions one per line followed by empty line:");
+        String line;
+        while ((line = in.readLine()) != null) {
+            line = line.trim();
+            if ("".equalsIgnoreCase(line)) {
+                return;
+            }
+            questionsPool.remove(line);
+        }
+    }
+
+    private void loadKnowledgeBase() {
+        try {
+            if (Files.exists(QUESTIONS_POOL_FILE)) {
+                String questionsPoolJson = Files.readString(QUESTIONS_POOL_FILE);
+                Gson gson = new Gson();
+                questionsPool.putAll(gson.fromJson(questionsPoolJson, new TypeToken<Map<String, Question>>() {
+                }.getType()));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    private void saveKnowledgeBase() {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(questionsPool);
+        try {
+            Files.writeString(QUESTIONS_POOL_FILE, json);
+            System.out.println("Questions pool saved to " + QUESTIONS_POOL_FILE.toAbsolutePath());
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
+}
